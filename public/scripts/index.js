75f6ba36f9107c87158eaa954fe39fabbd60115b
Любомир Славов <lubomir93x@gmail.com>
2016-01-06 21:48:27 +0200

    Navbar fixed
    Active class is now moving in the correct box.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by Lyubomir on 28/09/2015.
  */
-jQuery(function($){
+jQuery(function ($) {
     var socket = io.connect();
 
     var mS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
@@ -11,95 +11,93 @@ jQuery(function($){
     var wishCheck = false;
 
 
-    setInterval(function(){
-    
-    var pathname = document.URL;
-    if (pathname.indexOf("#") > -1) {
-     target = pathname.substr(pathname.indexOf("#")+1,pathname.length);
-    }else{
-      target = "index"    
+    setInterval(function () {
+
+        var pathname = document.URL;
+        if (pathname.indexOf("#") > -1) {
+            target = pathname.substr(pathname.indexOf("#") + 1, pathname.length);
+        } else {
+            target = "index"
         }
 
-    if(target !=currentTarget){
-        socket.emit("requestContent",target);
-        currentTarget = target; 
+        if (target != currentTarget) {
+            socket.emit("requestContent", target);
+            currentTarget = target;
         }
 
 
-},60);
+    }, 60);
 
 
-socket.on("retrieveContent",function(data){
-$("#content").empty();
-$("#content").append(data);
-if(currentTarget == "index"){
-    socket.emit('requestWishes'); 
-    
-}
+    $("#navbar li").click(function(){
 
+       $(this).siblings().removeClass("active");
+       $(this).addClass("active");
 
-
-});
+    });
 
 
 
 
 
-
-    $("#name").keyup(function(){
-       if($(this).val().trim() != ''){
-           nameCheck=true;
-           $(this).closest(".form-group").removeClass("has-error").addClass("has-success");
-       }else{
-           nameCheck=false;
-           $(this).closest(".form-group").removeClass("has-success").addClass("has-error");
-       }
+    $("#name").keyup(function () {
+        if ($(this).val().trim() != '') {
+            nameCheck = true;
+            $(this).closest(".form-group").removeClass("has-error").addClass("has-success");
+        } else {
+            nameCheck = false;
+            $(this).closest(".form-group").removeClass("has-success").addClass("has-error");
+        }
         unlockButton();
     });
-    $("#wishText").keyup(function(){
-        if($(this).val().trim() != ''){
-            wishCheck=true;
+    $("#wishText").keyup(function () {
+        if ($(this).val().trim() != '') {
+            wishCheck = true;
             $(this).closest(".form-group").removeClass("has-error").addClass("has-success");
-        }else{
-            wishCheck=false;
+        } else {
+            wishCheck = false;
             $(this).closest(".form-group").removeClass("has-success").addClass("has-error");
         }
         unlockButton();
     });
 
-function unlockButton(){
-    if(nameCheck && wishCheck){
-    $("#wishButton").removeAttr("disabled");
-    }else{
-    $("#wishButton").attr("disabled",true);
-    }
 
-}
 
-    $('#send-message').submit(function(e){
+    $('#send-message').submit(function (e) {
         var today = new Date();
-        var time = [ today.getDate(), mS[today.getMonth()],today.getFullYear()];
-        socket.emit('new wish', [$("#name").val(), $("#wishText").val() , time] );
+        var time = [today.getDate(), mS[today.getMonth()], today.getFullYear()];
+        socket.emit('new wish', [$("#name").val(), $("#wishText").val(), time]);
         $("#wishText").val('');
         $("#name").val('');
     });
 
-    socket.on('update wishes', function(data){
+    socket.on('update wishes', function (data) {
 
-        var newMsg ="<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>"+ data.msg + "</p><small>" + data.nick + "," +  data.time + "</small></blockquote></div>";
+        var newMsg = "<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>" + data.msg + "</p><small>" + data.nick + "," + data.time + "</small></blockquote></div>";
 
-        var newMsg ="<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>"+ data.msg + "</p><small> " + data.nick + ", " +  data.time + "</small></blockquote></div>";
+        var newMsg = "<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>" + data.msg + "</p><small> " + data.nick + ", " + data.time + "</small></blockquote></div>";
 
         $('#wishTable').append(newMsg);
     });
 
-    socket.on('retrieveWishes',function(data){
+    socket.on("retrieveContent", function (data) {
+        $("#content").empty();
+        $("#content").append(data);
+        if (currentTarget == "index") {
+            socket.emit('requestWishes');
 
-        data.Messages.forEach(function(item){
+        }
 
-            var newMsg ="<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>"+ item.msg + "</p><small>" + item.nick + "," +  item.time + "</small></blockquote></div>";
 
-            var newMsg ="<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>"+ item.msg + "</p><small> " + item.nick + ", " +  item.time + "</small></blockquote></div>";
+    });
+
+    socket.on('retrieveWishes', function (data) {
+
+        data.Messages.forEach(function (item) {
+
+            var newMsg = "<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>" + item.msg + "</p><small>" + item.nick + "," + item.time + "</small></blockquote></div>";
+
+            var newMsg = "<div class='col-xs-10 col-xs-offset-1 wishItem'><blockquote><p>" + item.msg + "</p><small> " + item.nick + ", " + item.time + "</small></blockquote></div>";
 
             $('#wishTable').append(newMsg);
         });
@@ -107,8 +105,18 @@ function unlockButton(){
     });
 
 
-
 });
 
+
+
+
+function unlockButton() {
+    if (nameCheck && wishCheck) {
+        $("#wishButton").removeAttr("disabled");
+    } else {
+        $("#wishButton").attr("disabled", true);
+    }
+
+}
 
 
